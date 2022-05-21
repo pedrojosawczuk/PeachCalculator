5b298894ac0b4c5f95efb82251dce5021fe14570
Pedro J de O Sawczuk <pedrojosawczuk@gmail.com>
2022-05-21 09:25:38 -0300

    Warning fix: class to className

--- src/context/Footer.js
+++ src/context/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   <>
     {/* Footer */}
     <footer>
-      Having issues with this website? Open a issue on <a class="link" href="https://github.com/pedrojosawczuk/PeachCalculator">GitHub</a>.<br/>
-      {/*Made by <a class="link" href="https://github.com/pedrojosawczuk">Pedro</a> with ❤️<br/>*/}
+      Having issues with this website? Open a issue on <a className="link" href="https://github.com/pedrojosawczuk/PeachCalculator">GitHub</a>.<br/>
+      {/*Made by <a className="link" href="https://github.com/pedrojosawczuk">Pedro</a> with ❤️<br/>*/}
       <a href="https://www.flaticon.com/free-icons/peach" title="peach icons">Peach icons created by Freepik - Flaticon</a>
     </footer>
   </>
