12431cb976ce0e10793c6d074b3cfe083bf49969
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-05-31 15:22:54 -0300

    add: screenshots

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 <p align="center">
-  <img align="center" width="100px" height="100px" src="src/logo.png" alt="Bot Picture">
+  <img align="center" width="100px" height="100px" src="src/logo.png">
   <h1 align="center">Peach Calculator<h3 align="center">Calculator of Equivalent Resistance, Potency, Current and Voltages over resistors R1, R2 and R3. 👨‍💻</h3>
 </p>
+
+## ScreenShot
+  <h5 align="center">Light / Dark</h5>
+  <img alt="Peach Light" width="49%" src="screenshots/light.png"/>
+  <img alt="Peach Dark" width="49%" src="screenshots/dark.png"/>
+
+  <h5 align="center">Result</h5>
+  
 
 ## Input
 
--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import Container from './Container';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+/*
+import LightSwitch from './context/LightSwitch';
+import { UserModeContext } from './context/UserMode';
 import { UserModeProvider } from './context/UserMode';
-import './App.css';
+*/
+import logo from './logo.png';
+import './index.css';
 
 function App() {
+  /*
+  const {userMode} = useContext(UserModeContext);
+    <UserModeProvider>
+  <LightSwitch />
+  
+    </UserModeProvider>
+  */
   return (
   <>
-    <UserModeProvider>
-      <Container />
-    </UserModeProvider>
+    <div className='container container-dark'/*className={userMode ? `container container-dark` : `container container-light`}*/>
+      <Header
+        logo={ logo }
+        title='Peach Calculator'
+        desc='Calculator of Equivalent Resistance, Potency, Current and Voltages over resistors R1, R2 and R3.'/>
+      <Content />
+      <Footer />
+    </div>
   </>
   );
 }
