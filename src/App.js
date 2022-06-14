2461c10d80b33156577d8b02aa0d631eee9d5013
Pedro J de O Sawczuk <89877096+pedrojosawczuk@users.noreply.github.com>
2022-06-14 16:40:19 -0300

    change: add dark mode and light mode css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
-/*
-import LightSwitch from './context/LightSwitch';
-import { UserModeContext } from './context/UserMode';
-import { UserModeProvider } from './context/UserMode';
-*/
 import logo from './logo.png';
 import './index.css';
 
 function App() {
-  /*
-  const {userMode} = useContext(UserModeContext);
-    <UserModeProvider>
-  <LightSwitch />
-  
-    </UserModeProvider>
-  */
   return (
   <>
-    <div className='container container-dark'/*className={userMode ? `container container-dark` : `container container-light`}*/>
+    <div className='container'>
       <Header
         logo={ logo }
         title='Peach Calculator' />
